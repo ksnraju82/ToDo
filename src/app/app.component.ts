27d8ab1e605008267bb71d7d5cb64c4258e69962
satya <ksnraju82@gmail.com>
2020-03-04 05:40:25 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-app';
+  todoArray=[];
+
+  @ViewChild('todo') nameInputRef: ElementRef;
+
+  addTodo(value){    
+    this.todoArray.push(value);
+  }
+  deleteItem($event){   
+    for(let i=0 ;i<= this.todoArray.length ;i++)
+    {
+      if(this.nameInputRef.nativeElement.value== this.todoArray[i])
+        {
+          this.todoArray.splice(i,1) 
+        }  
+    } 
+  }
+  todoSubmit(value?:any){ console.log(value)  }
+}
+
